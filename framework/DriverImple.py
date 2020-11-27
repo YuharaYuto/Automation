08a1dc521f4699ecfd67b6d6f8797057ba593758
yuhara <yuto.com>
2020-11-27 21:37:30 +0900

    add driverimple

--- framework/DriverImple.py
+++ framework/DriverImple.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException,NoSuchElementException
+from selenium.webdriver.common.alert import Alert
+from businessException import BusinessException
 import time
 import os
 import signal
@@ -25,47 +27,85 @@ class DriverImple(Driver):
         pass
 
     def pushButton(self, XPath):
-        try:
-            wait = WebDriverWait(self.driver, 10)
-            wait.until(EC.element_to_be_clickable((By.XPATH, XPath)))
-            button = self.driver.find_element_by_xpath(XPath)
-            button.click()
-        except NoSuchElementException:
-            print("対象の要素が見つかりませんでした。")
-            raise Exception
-        except TimeoutException:
-            print("対象の要素が時間内に見つかりませんでした。")
-            raise Exception
-        pass
+        for i in range(1,5):
+            try:
+                wait = WebDriverWait(self.driver, 10)
+                wait.until(EC.element_to_be_clickable((By.XPATH, XPath)))
+                button = self.driver.find_element_by_xpath(XPath)
+                button.click()
+            except NoSuchElementException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が見つかりませんでした。¥n")
+                pass
+            except TimeoutException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が時間内に見つかりませんでした。¥n")
+                pass
+            else:
+                return
+        return
 
     def inputInfo(self, XPath,info):
-        try:
-            wait = WebDriverWait(self.driver, 10)
-            wait.until(EC.element_to_be_clickable((By.XPATH, XPath)))
-            inputBox = self.driver.find_element_by_xpath(XPath)
-            inputBox.send_keys(info)
-        except NoSuchElementException:
-            print("対象の要素が見つかりませんでした。")
-            raise Exception
-        except TimeoutException:
-            print("対象の要素が時間内に見つかりませんでした。")
-            raise Exception
-            pass
-        pass
+        for i in range(1,5):
+            try:
+                wait = WebDriverWait(self.driver, 10)
+                wait.until(EC.element_to_be_clickable((By.XPATH, XPath)))
+                inputBox = self.driver.find_element_by_xpath(XPath)
+                inputBox.send_keys(info)
+            except NoSuchElementException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が見つかりませんでした。¥n")
+                pass
+            except TimeoutException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が時間内に見つかりませんでした。¥n")
+                pass
+            else:
+                return
+        return
 
     def clearInfo(self,XPath):
-        try:
-            wait = WebDriverWait(self.driver, 10)
-            wait.until(EC.element_to_be_clickable((By.XPATH, XPath)))
-            box = self.driver.find_element_by_xpath(XPath)
-            box.clear()
-        except NoSuchElementException:
-            print("対象の要素が見つかりませんでした。")
-            raise Exception
-        except TimeoutException:
-            print("対象の要素が時間内に見つかりませんでした。")
-            raise Exception
-            pass
+        for i in range(1,5):
+            try:
+                wait = WebDriverWait(self.driver, 10)
+                wait.until(EC.element_to_be_clickable((By.XPATH, XPath)))
+                box = self.driver.find_element_by_xpath(XPath)
+                box.clear()
+            except NoSuchElementException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が見つかりませんでした。¥n")
+                pass
+            except TimeoutException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が時間内に見つかりませんでした。¥n")
+                pass
+            else:
+                return
+        return
+
+    def checkRadio(self, XPath):
+        for i in range(1,5):
+            try:
+                wait = WebDriverWait(self.driver, 10)
+                wait.until(EC.element_to_be_clickable((By.XPATH, XPath)))
+                radio = self.driver.find_element_by_xpath(Xpath)
+                radio.click()
+            except NoSuchElementException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が見つかりませんでした。¥n")
+                pass
+            except TimeoutException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が時間内に見つかりませんでした。¥n")
+                pass
+            except:
+                if i == 4:
+                    raise Exception
+                pass
+            else:
+                return
+        return
+
 
     def closeBrowser(self):
         self.driver.close()
@@ -75,20 +115,120 @@ class DriverImple(Driver):
         os.kill(self.driver.service.process.pid,signal.SIGTERM)
     
     def waitElementByDisplay(self, XPath):
-        wait = WebDriverWait(self.driver, 180)
-        wait.until(EC.element_to_be_clickable((By.XPATH, XPath)))
-        try:
-            wait = WebDriverWait(self.driver, 180)
-            wait.until(EC.element_to_be_clickable((By.XPATH, XPath)))
-        except NoSuchElementException:
-            print("対象の要素が見つかりませんでした。")
-            raise Exception
-        except TimeoutException:
-            print("対象の要素が時間内に見つかりませんでした。")
-            raise Exception
-        pass
+        for i in range(1,5):
+            try:
+                wait = WebDriverWait(self.driver, 10)
+                wait.until(EC.element_to_be_clickable((By.XPATH, XPath)))
+            except NoSuchElementException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が見つかりませんでした。¥n")
+                pass
+            except TimeoutException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が時間内に見つかりませんでした。¥n")
+                pass
+            else:
+                return
+        return
+
+    def acceptAlert(self):
+        for i in range(1,5):
+            try:
+                Alert(self.driver).accept()
+            except:
+                traceback.print_exc()
+            else:
+                return
+            
+        return
 
     def changeNextTab(self):
-        handle_array = self.driver.window_handles
-        self.driver.switch_to.window(handle_array[1])
+        for i in range(1,5):
+            try:
+                handle_array = self.driver.window_handles
+                self.driver.switch_to.window(handle_array[1])
+                pass
+            except NoSuchElementException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が見つかりませんでした。¥n")
+                pass
+            except TimeoutException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が時間内に見つかりませんでした。¥n")
+                pass
+            except:
+                if i == 4:
+                    raise Exception
+                pass
+            else:
+                return
+        return
+        
+    def changeLastTab(self):
+        for i in range(1,5):
+            try:
+                handle_array = self.driver.window_handles
+                self.driver.switch_to.window(handle_array[-1])
+                pass
+            except NoSuchElementException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が見つかりませんでした。¥n")
+                pass
+            except TimeoutException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が時間内に見つかりませんでした。¥n")
+                pass
+            except:
+                if i == 4:
+                    raise Exception
+                pass
+            else:
+                return
+        return
+
+    def executeScript(self, script):
+        for i in range(1,5):
+            try:
+                self.driver.execute_script(script)
+            except NoSuchElementException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が見つかりませんでした。¥n")
+                pass
+            except TimeoutException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が時間内に見つかりませんでした。¥n")
+                pass
+            except:
+                if i == 4:
+                    raise Exception
+                #raise Exception
+                pass
+            else:
+                return
+        return
+
+        
+
+    def getText(self, Xpath):
+        for i in range(1,5):
+            try:
+                res = self.driver.find_element_by_xpath(Xpath).text
+                return res
+            except NoSuchElementException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が見つかりませんでした。¥n")
+                pass
+            except TimeoutException:
+                if i == 4:
+                    raise BusinessException("対象の要素" + XPath + "が時間内に見つかりませんでした。¥n")
+                pass
+            except:
+                if i == 4:
+                    pass
+                    raise Exception
+                pass
+            else:
+                return
+        return
+        
 
